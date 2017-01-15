4a0fe737ce6415dfd44c259734c8dd21b4569fcc
Max Woollard <maxwoollard@Maxs-MacBook-Pro.local>
2017-01-15 20:46:33 +0000

    Refactor error messages and remove redundant styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,101 +1,35 @@
-$(document).ready(function(){
+/* global $ */
+'use strict';
 
-	var requiredAttributes =  {
-		$favouriteMusic: 'contest1_name',
-		$firstName: 'first_name',
-		$country: 'country',
-		$email: 'email',
-		$birthDay: 'birth_day',
-		$birthMonth: 'birth_month',
-		$birthYear: 'birth_year',
-		$privacyCheck: 'privacy_check'
-	},
-	$submit = $('.submit');
+$(document).ready(function() {
+  var requiredAttributes = {
+    $favouriteMusic: 'contest1_name',
+    $firstName: 'first_name',
+    $country: 'country',
+    $email: 'email',
+    $birthDay: 'birth_day',
+    $birthMonth: 'birth_month',
+    $birthYear: 'birth_year',
+    $privacyCheck: 'privacy_check'
+  };
+  var $submit = $('.submit');
 
-	// hide form errors on page load
-	$('.form-error').hide();
-	$('.invalid-email-error').hide();
+  // click function for validation
+  $submit.on('click', function() {
+    $.each(requiredAttributes, function(key, value) {
+      var attributeName = $('[name="' + value + '"]');
 
-	// click function for validation
-	$submit.on('click', function(){
-
-		// get values for all required fields
-		var favouriteMusic = $("[name='contest1_name']").val();
-		var firstName = $("[name='first_name']").val();
-		var country = $("[name='country']").val();
-		var email = $("[name='email']").val();
-		var birthDay = $("[name='birth_day']").val();
-		var birthMonth = $("[name='birth_month']").val();
-		var birthYear = $("[name='birth_year']").val();
-		var privacyCheck = $("[name='privacy_check']")
-
-		// loop over required attributes object to get all attribute names
-		$.each( requiredAttributes, function( key, value ) {
-		  
-		  // save looped attributes in a variable
-		  var attributeName = $("[name='" + value + "']");
-		  var fieldName = value;
-
-		  // switch statement to add errors to all empty fields
-		  switch(fieldName){
-		  	case '' + fieldName + '':
-		  		if (attributeName.val() === '') {
-		  			$("[name='" + value + "']").addClass('error');
-		  			$('.form-error').show();
-		  		} else if (attributeName.val()) {
-		  			$("[name='" + value + "']").removeClass('error');
-		  		}
-		    	break;
-		  }
-		})
-
-		// remove all errors if the field has a value
-		if (favouriteMusic) {
-		  $('.favourite-music.form-error').hide();
-		}
-
-		if (firstName) {
-		  $('.first-name.form-error').hide();
-		}
-
-		if (country) {
-		  $('.country.form-error').hide();
-		}
-
-		if (email) {
-		  $('.email.form-error').hide();
-		}
-		
-		// if(IsEmail(email) == false){
-  //     $('.invalid-email-error').show();
-  //   }
-
-		if (birthDay) {
-		  $('.day-of-birth.form-error').hide();
-		}
-
-		if (birthMonth) {
-		  $('.month-of-birth.form-error').hide();
-		}
-
-		if (birthYear) {
-		  $('.year-of-birth.form-error').hide();
-		}
-
-		if (privacyCheck.is(':checked')) {
-		  $('.privacy-check.form-error').hide();
-		}
-	});
-
-	// function IsEmail(email) {
-	//   var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	//   if (!regex.test(email)) {
-	//     return false;
-	//   } else {
-	//     return true;
-	//   }
-	// }
-	
+      if (attributeName.val() === '') {
+        $('[name="' + value + '"]')
+          .addClass('error')
+          .prev()
+          .addClass('js-show');
+      } else if (attributeName.val()) {
+        $('[name="' + value + '"]')
+          .removeClass('error')
+          .prev()
+          .removeClass('js-show');
+      }
+    });
+  });
 });
-
-
