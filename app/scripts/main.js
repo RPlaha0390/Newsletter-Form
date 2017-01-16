8001d4c9f0fe6b319b83a9b24c981be4233847a7
Raman <raman.plaha@brandwidth.com>
2017-01-16 16:54:32 +0000

    adding email validation and date of birth validation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,8 @@
 'use strict';
 
 $(document).ready(function() {
-  var requiredAttributes = {
+  var 
+  	requiredAttributes = {
     $favouriteMusic: 'contest1_name',
     $firstName: 'first_name',
     $country: 'country',
@@ -11,11 +12,18 @@ $(document).ready(function() {
     $birthMonth: 'birth_month',
     $birthYear: 'birth_year',
     $privacyCheck: 'privacy_check'
-  };
-  var $submit = $('.submit');
+  },
+  $submit = $('.submit');
 
   // click function for validation
   $submit.on('click', function() {
+
+  	var 
+  		$email = $('input[name="email"]'),
+  		$day = $('select[name="birth_day"]'), 
+  		$month = $('select[name="birth_month"]'),
+  		$year = $('select[name="birth_year"]');
+
     $.each(requiredAttributes, function(key, value) {
       var attributeName = $('[name="' + value + '"]');
 
@@ -31,5 +39,42 @@ $(document).ready(function() {
           .removeClass('js-show');
       }
     });
+
+    function getAge(dateString) {
+      var 
+      	today = new Date(),
+      	birthDate = new Date(dateString),
+      	yearAge = today.getFullYear() - birthDate.getFullYear(),
+      	month = today.getMonth() - birthDate.getMonth();
+
+      if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+          yearAge--;
+      }
+      return yearAge;
+    }
+
+    if (getAge($year.val() + '/' + $month.val() + '/' + $day.val()) < 14) {
+    	$year.addClass('error');
+    	$month.addClass('error');
+    	$day.addClass('error');
+    	$('.age-error').addClass('js-show');
+    } else {
+    	$('.age-error').removeClass('js-show');
+    }
+
+    function ValidateEmail(email) {
+      var expr = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return expr.test(email);
+    };
+
+    if ((!ValidateEmail($email.val())) && $email.val()) {
+      $email.addClass('error');
+      $('.email-error').addClass('js-show');
+    }
+    else {
+    	$email.removeClass('error');
+    	$('.email-error').removeClass('js-show');
+    }
+
   });
 });
